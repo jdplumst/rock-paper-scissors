acacf58d2d1564ed4ba2eff9d1ef9803ebe18350
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2022-10-17 05:20:35 -0400

    Add playRound function

--- game.js
+++ game.js
@@ -8,3 +8,35 @@ function getComputerChoice() {
         return 'Scissors'
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toUpperCase()
+    if (playerSelection == 'ROCK' && computerSelection == 'Rock') {
+        console.log('It\'s a draw! You both played rock!')
+        return 0
+    } else if (playerSelection == 'ROCK' && computerSelection == 'Paper') {
+        console.log('You lose! Paper beats rock!')
+        return -1
+    } else if (playerSelection == 'ROCK' && computerSelection == 'Scissors') {
+        console.log('You win! Rock beats scissors!')
+        return 1
+    } else if (playerSelection == 'PAPER' && computerSelection == 'Rock') {
+        console.log('You win! Paper beats rock!')
+        return 1
+    } else if (playerSelection == 'PAPER' && computerSelection == 'Paper') {
+        console.log('It\'s a draw! You both played paper!')
+        return 0
+    } else if (playerSelection == 'PAPER' && computerSelection == 'Scissors') {
+        console.log('You lose! Scissors beats paper!')
+        return -1
+    } else if (playerSelection == 'SCISSORS' && computerSelection == 'Rock') {
+        console.log('You lose! Rock beats scissors!')
+        return -1
+    } else if (playerSelection == 'SCISSORS' && computerSelection == 'Paper') {
+        console.log('You win! Scissors beats paper!')
+        return 1
+    } else if (playerSelection == 'SCISSORS' && computerSelection == 'Scissors') {
+        console.log('It\'s a draw! you both played scissors!')
+        return 0
+    }
+}
